0243e812a5e937f7b39acb85b5a1b8d3d51ffa8a
nicolas varone <varonenico@gmail.com>
2022-05-17 21:39:38 +0200

    Popular

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ function scrollHeader() {
   else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper('.popular__container', {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
